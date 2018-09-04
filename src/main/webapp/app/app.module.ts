61b0febd29a2e3c86c65ded4530d4376c6c5c976
IamGiel <november14giel@gmail.com>
2018-09-04 13:04:48 -0400

    Initial application generated by JHipster-5.3.0

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,69 @@
+import './vendor.ts';
+
+import { NgModule, Injector } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgbDatepickerConfig } from '@ng-bootstrap/ng-bootstrap';
+import { Ng2Webstorage, LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+import { JhiEventManager } from 'ng-jhipster';
+
+import { AuthInterceptor } from './blocks/interceptor/auth.interceptor';
+import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
+import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
+import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
+import { JhipsterSharedModule } from 'app/shared';
+import { JhipsterCoreModule } from 'app/core';
+import { JhipsterAppRoutingModule } from './app-routing.module';
+import { JhipsterHomeModule } from './home/home.module';
+import { JhipsterAccountModule } from './account/account.module';
+import { JhipsterEntityModule } from './entities/entity.module';
+import * as moment from 'moment';
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+import { JhiMainComponent, NavbarComponent, FooterComponent, PageRibbonComponent, ActiveMenuDirective, ErrorComponent } from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        JhipsterAppRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-' }),
+        JhipsterSharedModule,
+        JhipsterCoreModule,
+        JhipsterHomeModule,
+        JhipsterAccountModule,
+        JhipsterEntityModule
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true,
+            deps: [LocalStorageService, SessionStorageService]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthExpiredInterceptor,
+            multi: true,
+            deps: [Injector]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorHandlerInterceptor,
+            multi: true,
+            deps: [JhiEventManager]
+        },
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: NotificationInterceptor,
+            multi: true,
+            deps: [Injector]
+        }
+    ],
+    bootstrap: [JhiMainComponent]
+})
+export class JhipsterAppModule {
+    constructor(private dpConfig: NgbDatepickerConfig) {
+        this.dpConfig.minDate = { year: moment().year() - 100, month: 1, day: 1 };
+    }
+}
